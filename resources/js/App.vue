7b628382c8ef333966127d00bf3f5a7693bdf34b
mkApproach <kinai-ap@cameo.plala.or.jp>
2022-04-20 13:28:55 +0900

    first commit

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <header>
-      <Navbar />
+       <Navbar />
     </header>
     <main>
-      <sidebar class="sidebar-area"></sidebar>
       <div class="scafold-wrapper text-center">
         <div class="container">
           <Message /> <!-- ★ 追加 -->
@@ -20,7 +19,6 @@
 import Message from './components/Message.vue'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
-import Sidebar from './components/Sidebar.vue'
 
 import { NOT_FOUND, UNAUTHORIZED, INTERNAL_SERVER_ERROR } from './util'
 
@@ -28,7 +26,6 @@ export default {
   components: {
     Message,
     Navbar,
-    Sidebar,
     Footer
   },
   computed: {
@@ -60,28 +57,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.sidebar-area {
-  margin-top: 0px;
-  /* 左側に固定 */
-  float: left;
-}
-
-.footer-area {
-  margin-top: 40px;
-}
-
-.scafold-wrapper {
-  /* display: flex; 要素を横並びにする */
-  flex-direction: column; /* 要素の並び順の主軸を指定 上 => 下 */
-  min-height: 100vh; /* 要素の高さの最小値を指定 vhはviewport(表示領域) heightの略 */
-
-  /* サイドバーのwidth分だけ範囲を削除 */
-  width: calc(100% - 200px);
-
-  /* サイドバーで隠れるので右に寄せる */
-  margin: 0 0 0 180px;
-}
-</style>
-
--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -4,6 +4,21 @@
       アプローチの ちいさな写真館
     </RouterLink>
     <div class="navbar__menu">
+      <div>
+        <div class="sidebar-select">
+            <select v-model="selectedKey" v-on:change="selected">
+                    <option v-for="(value, key) in items">
+                        {{ key }}
+                    </option>
+            </select>
+           
+            <select>
+                <option v-if="selectedItem" v-for="item in selectedItem">
+                    {{ item.name }}
+                </option>
+            </select>  
+        </div>
+      </div>
       <div v-if="isLogin" class="navbar__item">
         <button class="button" @click="showForm = ! showForm">
           <i class="icon ion-md-add"></i>
@@ -31,8 +46,33 @@ export default {
   },
   data () {
     return {
-      showForm: false
+      showForm: false,
+
+      selectid: '',
+      selectedKey: '',
+      selectedItem: '',
+      items: {
+          ユニフォーム: [
+              { name: '野　球' },
+              { name: 'サッカー' },
+              { name: 'フットサル' },
+              { name: 'マラソン' },
+              { name: 'バレーボール' },
+          ],
+          その他: [
+              { name: 'マスク' },
+              { name: 'タオル' },
+              { name: '抱き枕' },
+              { name: 'クッション' },
+              { name: 'Fit' },
+          ],
+      }
     }
+  },
+  methods:{
+      selected: function(){
+          this.selectedItem = this.items[this.selectedKey];
+      }
   },
   computed: {
     isLogin () {
